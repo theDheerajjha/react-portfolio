c363dc59a7c281fd7b89af34d95cd337ff50e61d
Dheeraj Kumar Jha <52864945+theDheerajjha@users.noreply.github.com>
2023-05-04 03:11:50 +0530

    implementing routing in portfolio

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
-
-import Navbar from './components/Navbar';
+import Navbar from "./components/Navbar";
 import "./style.css";
-import './App.css';
+import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Home from './components/Home';
+import Home from "./components/Home";
+import AboutMe from "./components/AboutMe";
+import Projects from "./components/Projects";
+import Resume from "./components/Resume";
 
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <Navbar/>
-      <Home/>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/project" element={<Projects />} />
+          <Route path="/about" element={<AboutMe />} />
+          <Route path="/resume" element={<Resume />} />
+          <Route path="*" element={<Navigate to="/"/>} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,28 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import { Link } from "react-router-dom";
 
 function NavBar() {
-
   return (
     <>
       <Navbar sticky="top" className="nav-bar" variant="light">
         <Container>
-          <Navbar.Brand href="#home">
-         
-          </Navbar.Brand>
+          <Navbar.Brand href="#home"></Navbar.Brand>
           <Nav className="ms-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">About Me</Nav.Link>
-            <Nav.Link href="#pricing">Resume</Nav.Link>
-         </Nav>
+            <Nav.Link as={Link} to="/Home">
+              Home
+            </Nav.Link>
+            <Nav.Link as={Link} to="/about">
+              About Me
+            </Nav.Link>
+            <Nav.Link as={Link} to="/resume">
+              Resume
+            </Nav.Link>
+            <Nav.Link as={Link} to="/project">
+              Projects
+            </Nav.Link>
+          </Nav>
         </Container>
       </Navbar>
     </>
