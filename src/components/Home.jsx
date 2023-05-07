b773a947231303f3679b3eccdd7215d91fefb63e
Dheeraj Kumar Jha <52864945+theDheerajjha@users.noreply.github.com>
2023-05-08 02:15:55 +0530

    WIP

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,27 +1,37 @@
 import React from "react";
+import Button from "react-bootstrap/Button";
 
 export default function Home() {
   return (
     <div className="intro-container">
-      <div className="intro-text">
-        <h1 className="intro-heading">Hi, I'm Dheeraj Jha</h1>
-        <p className="intro-subheading">
-          {/* <h2>
+      <section className="intro-text">
+        <div>
+          <h2 className="intro-heading">
+            Hi, I'm <span className="full-name">Dheeraj Jha</span>
+          </h2>
+          <p className="intro-subheading">
+            {/* <h2>
             <span className="engineer">Frontend</span> {"<"} Engineer {"/"}
             {">"}
           </h2> */}
-          <h2>
-            <span className="engineer">Frontend </span> Engineer
-          </h2>
-          With a passion for creating User-friendly Web Experiences.
-        </p>
-      </div>
-      <div className="intro-image">
-        <img
-          src="https://avatars.githubusercontent.com/u/52864945?v=4"
-          alt="Profile picture"
-        />
-      </div>
+            <h2>
+              <span className="engineer">Frontend </span> Engineer
+            </h2>
+            With a passion for creating User-friendly Web Experiences.
+          </p>
+        </div>
+        <div className="mb-2 intro-buttons">
+          <Button onClick={() => window.location = 'mailto:write4dheeraj@gmail.com'} variant="primary" size="lg">
+          &nbsp; &nbsp;Hire Me&nbsp; &nbsp;{" "}
+          </Button>{" "}
+          <Button variant="secondary" size="lg">
+          &nbsp; &nbsp;Resume&nbsp; &nbsp;{" "}
+          </Button>
+        </div>
+      </section>
+      <section className="intro-image-section   ">
+        <img src="https://i.ibb.co/59CR5Wz/DP.jpg" alt="Profile picture" />
+      </section>
     </div>
   );
 }
--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,7 +1,59 @@
 import React from "react";
+import Button from "react-bootstrap/Button";
 
-export default function Resume(){
-    return(
-        <p>Resume</p>
-    )
+export default function Resume() {
+  return (
+    // <div className="about-me">
+    //   <div class="card">
+    //     <div class="card-body skill-section">
+    //       <h2 class="card-title">Skills</h2>
+    //       <div class="row">
+    //         <div class="col-md-4">
+    //           <div class="skill text-center">
+    //             <img
+    //               src="https://upload.wikimedia.org/wikipedia/commons/9/95/Vue.js_Logo_2.svg"
+    //               class="img-fluid"
+    //               alt="Vue"
+    //             />
+    //           </div>
+    //         </div>
+    //         <div class="col-md-4">
+    //           <div class="skill text-center">
+    //             <img
+    //               src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg"
+    //               class="img-fluid"
+    //               alt="React"
+    //             />
+    //           </div>
+    //         </div>
+    //         <div class="col-md-4">
+    //           <div class="skill text-center">
+    //             <img
+    //               src="https://upload.wikimedia.org/wikipedia/commons/c/cf/Angular_full_color_logo.svg"
+    //               class="img-fluid"
+    //               alt="Angular"
+    //             />
+    //           </div>
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
+    <div className="resume-section">
+      <iframe
+        className="resume-frame"
+        src="https://drive.google.com/file/d/1-yo-W8dS0oIvdcGVTrKMhR_nY3_bFnU1/preview"
+        width="50%"
+        height="385"
+        allow="autoplay"
+      ></iframe>{" "}
+      <Button
+        variant="secondary"
+        size="lg"
+        href="https://drive.google.com/file/d/1-yo-W8dS0oIvdcGVTrKMhR_nY3_bFnU1/view"
+      >
+        Download
+      </Button>
+    </div>
+  );
 }
