125e2de000df3cefc9cc559ba195ce7fbc209a86
Dheeraj Kumar Jha <52864945+theDheerajjha@users.noreply.github.com>
2023-05-20 03:54:50 +0530

    Addedd social media links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,13 +3,13 @@ const Footer = () => {
   return (
     <footer className="footer-container">
       <div className="social-icons">
-        <a href="[insert link to social media profile]" target="_blank" rel="noopener noreferrer">
+        <a href="https://github.com/theDheerajjha" target="_blank" rel="noopener noreferrer">
           <i className="fab fa-github"></i>
         </a>
-        <a href="[insert link to social media profile]" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.linkedin.com/in/dheeraj-jha-65b2231b7" target="_blank" rel="noopener noreferrer">
           <i className="fab fa-linkedin"></i>
         </a>
-        <a href="[insert link to social media profile]" target="_blank" rel="noopener noreferrer">
+        <a href="https://twitter.com/write4dheeraj" target="_blank" rel="noopener noreferrer">
           <i className="fab fa-twitter"></i>
         </a>
       </div>
