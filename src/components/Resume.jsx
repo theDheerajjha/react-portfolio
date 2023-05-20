75b09e5559c7168392a6ac3e5aade24c76e3d865
Dheeraj Kumar Jha <52864945+theDheerajjha@users.noreply.github.com>
2023-05-20 23:56:26 +0530

    Removed fredundant code from Resume component

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -3,42 +3,6 @@ import Button from "react-bootstrap/Button";
 
 export default function Resume() {
   return (
-    // <div className="about-me">
-    //   <div class="card">
-    //     <div class="card-body skill-section">
-    //       <h2 class="card-title">Skills</h2>
-    //       <div class="row">
-    //         <div class="col-md-4">
-    //           <div class="skill text-center">
-    //             <img
-    //               src="https://upload.wikimedia.org/wikipedia/commons/9/95/Vue.js_Logo_2.svg"
-    //               class="img-fluid"
-    //               alt="Vue"
-    //             />
-    //           </div>
-    //         </div>
-    //         <div class="col-md-4">
-    //           <div class="skill text-center">
-    //             <img
-    //               src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg"
-    //               class="img-fluid"
-    //               alt="React"
-    //             />
-    //           </div>
-    //         </div>
-    //         <div class="col-md-4">
-    //           <div class="skill text-center">
-    //             <img
-    //               src="https://upload.wikimedia.org/wikipedia/commons/c/cf/Angular_full_color_logo.svg"
-    //               class="img-fluid"
-    //               alt="Angular"
-    //             />
-    //           </div>
-    //         </div>
-    //       </div>
-    //     </div>
-    //   </div>
-    // </div>
     <div className="resume-section">
       <iframe
         className="resume-frame"
