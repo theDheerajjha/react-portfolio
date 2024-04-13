9ccd84ec9ceeabc50b4fcc0c5d2e064f5a46e850
Dheeraj Kumar Jha <52864945+theDheerajjha@users.noreply.github.com>
2024-04-14 01:56:38 +0530

    Hiding Navbar for now

--- src/App.js
+++ src/App.js
@@ -45,7 +45,7 @@ function App() {
     return (
       <Router>
         <div className="App">
-          <Navbar />
+          {/* <Navbar /> */}
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/project" element={<Projects />} />
